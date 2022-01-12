734b7ad39efa2553ae499efdb88dc507ab48f462
Sultan,Bilal B <b.sultan@student.fontys.nl>
2022-01-12 07:26:02 +0100

    URL DB

--- create_db.py
+++ create_db.py
@@ -5,8 +5,8 @@ def create_talbes():
     # User_role = Role(role_name = "User")
     hash_pw = generate_password_hash('Admin123',"sha256")
     x = User(first_name = 'Bilal', last_name = 'Sultan', email='bilal@sultan.com', hash_pw= hash_pw, role = admin_role)
-    db.session.add(admin_role)
-    db.session.add(User_role)
+    # db.session.add(admin_role)
+    # db.session.add(User_role)
     db.session.add(x)
     db.session.commit()
 
